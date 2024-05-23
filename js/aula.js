2110d91dbcde9315801ecbe55d626972c2de339f
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2024-05-23 17:47:51 -0300

    AulaJS

--- js/aula.js
+++ js/aula.js
@@ -1,11 +1,11 @@
-let val, pag, lit;
-
-val = parseFloat(prompt("Informe o valor do litro da gasolina:"));
-pag = parseFloat(prompt("Qual o valor em reais deseja abastecer?"))
-lit = pag/val;
-
-if(pag>0){
-    alert(`Você conseguirá abastecer ${lit.toFixed(1)}L de gasolina com ${pag} reais`);
-} else{
-    alert(`Não é possível abastecer...`)
+let nome, nota1, nota2, media, resposta;
+nome = prompt("Digite seu nome");
+nota1 = parseFloat(prompt("Digite a primeira nota"));
+nota2 = parseFloat(prompt("Digite a segunda nota"));
+media = (nota1 + nota2) / 2;
+if (media >= 7) {
+    resposta = "o aluno passou!";
+} else {
+    resposta = "aluno tirou nota baixa, reprovou e dançou";
 }
+alert(`${nome}, sua média do aluno é ${media.toFixed(1)}, ${resposta}`);
