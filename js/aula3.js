27b951ac4b16ea255de829422091513086660ae3
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2024-05-23 17:29:10 -0300

    AulaJS

--- js/aula3.js
+++ js/aula3.js
@@ -1,1 +1,7 @@
+let vei, dis, tem, veM;
+vei = prompt("Informe o modelo de seu veículo:");
+dis = parseFloat(prompt(`Informe qual a distância percorrida por ele?`));
+tem = parseFloat(prompt(`Informe em quanto tempo ele percorreu esta distância:`));
+veM = dis/tem;
 
+alert(`O modelo de seu veículo é ${vei}, ele percorreu ${dis}Km neste tempo: ${tem}. Sua velocidade média foi de ${veM.toFixed(2)}Km/h.`);
