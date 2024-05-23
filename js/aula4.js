0022b3b44f98337bd67b08e7cf2907e03b9ee564
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2024-05-23 17:41:11 -0300

    AulaJS

--- js/aula4.js
+++ js/aula4.js
@@ -8,15 +8,15 @@ a = "Adulto."
 resultado = "Você está classificado na categoria: ";
 
 if(idade >= 5 && idade <= 7){
-    alert(${resultado} ${ia});
+    alert(`${resultado} ${ia}`);
 } else if(idade >= 8 && idade <= 10){
-    alert(${resultado} ${ib});
+    alert(`${resultado} ${ib}`);
 } else if(idade >= 11 && idade <= 13){
-    alert(${resultado} ${ja});
+    alert(`${resultado} ${ja}`);
 } else if(idade >= 14 && idade <= 17){
-    alert(${resultado} ${jb});
+    alert(`${resultado} ${jb}`);
 } else if(idade >= 18){
-    alert(${resultado} ${a});
+    alert(`${resultado} ${a}`);
 } else {
     alert(`Não é possível classificá-lo...`)
 }
