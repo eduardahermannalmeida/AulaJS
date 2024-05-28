702e8eaf3e506cfdd0907a8f071faca2f33a5bdb
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2024-05-28 17:10:46 -0300

    AulaJS

--- js/aula7.js
+++ js/aula7.js
@@ -1,9 +1,18 @@
+
+function pegarValor() {
+    const nomeProd = document.getElementById('nome');
+    const reais = document.getElementById('real');
+    mostrarResultado(nomeProd.value, reais.value);
+}
+
 function converterDolar(valor) {
-    let dolar = valor*5.16;
+    let dolar = valor / 5.16;
     return dolar;
 }
 
-function mostrarResultado(valor){
-    document.write(`O valor convertido para dólar é 
-    ${converterDolar(valor)}`)
+function mostrarResultado(nome, valor) {
+    document.write(`O valor do produto ${nome} em reais 
+    é ${valor}, convertido para dólar é 
+    $${converterDolar(valor).toFixed(2)}.`);
 }
+
