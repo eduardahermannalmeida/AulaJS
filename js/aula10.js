e061e46ae5f77a1a4322f917fca13ca3212848d0
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2024-06-06 17:21:07 -0300

    AulaJS

--- js/aula10.js
+++ js/aula10.js
@@ -1,10 +1,53 @@
-function pegarValor() {
-    const minusculo1 = document.getElementById('minus1');
-    const minusculo2 = document.getElementById('minus2');
-    const maisculo1 = document.getElementById('maius1');
-    const maisculo2 = document.getElementById('maius2');
-    const palavra1 = document.getElementById('pal1');
-    const palavra2 = document.getElementById('pal2');
-    const texto = document.getElementById('tex');
-    mostrarResultado(minus1.value, minus2.value, maius1.value, maius2.value, pal1.value, pal2.value, tex.value);
+function pegarMinus1() {
+    const minus1 = document.getElementById('minusculo1');
+    transMaius1(minus1.value);
+}
+
+function transMaius1(texMinus1) {
+    const minus1Res = document.getElementById('maiusculo1');
+    minus1Res.value = texMinus1.toUpperCase();
+}
+
+function pegarMaius2() {
+    const maius2 = document.getElementById('maiusculo2');
+    transMaius2(maius2.value);
+}
+
+function transMaius2(texMaius2) {
+    const minus2Res = document.getElementById('minusculo2');
+    minus2Res.value = texMaius2.toLowerCase();
+}
+
+function pegarPal1() {
+    const pal1 = document.getElementById('palavra1');
+    contaCaracter(pal1.value);
+
+}
+
+function contaCaracter(texPal) {
+    const res = document.getElementById('resultado');
+    res.innerHTML = `A palavra ${texPal} tem ${texPal.length} caracter(es)`;
+}
+
+function pegarPal2() {
+    const pal2 = document.getElementById('palavra2');
+    trocaA(pal2.value);
+}
+
+function trocaA(texPal2) {
+    const pal2Res = document.getElementById('palavraComATrocado');
+    pal2Res.value = texPal2.replace(/a|A/g, "*");
+
+}
+
+function pegarTex() {
+    const tex = document.getElementById('texto');
+    semEspaco(tex.value);
+
+}
+
+function semEspaco(texSeEs) {
+    const texRes = document.getElementById('textoSemEspaco');
+    texRes.value = texSeEs.replace(/\s/g, '');
+
 }
