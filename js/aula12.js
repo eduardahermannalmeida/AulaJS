9c39b38a6101b8655c0f4a45470d344ce2216442
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2024-06-11 17:45:26 -0300

    AulaJS

---
+++ js/aula12.js
@@ -0,0 +1,33 @@
+var arr = [];
+
+function addItem() {
+    if (localStorage.meuArr) {
+        arr = JSON.parse(localStorage.getItem('meuArr'));
+    }
+    let novoItem = document.getElementById('v').value;
+    arr.push(novoItem);
+    document.getElementById('v').value = " ";
+    localStorage.meuArr = JSON.stringify(arr);
+
+}
+
+function showItems() {
+    let resultDiv = document.getElementById('d');
+    resultDiv.innerHTML = " ";
+    if (localStorage.meuArr) {
+        arr = JSON.parse(localStorage.getItem('meuArr'));
+    }
+    for (var i in arr) {
+        let p = document.createElement('p');
+        p.innerHTML = arr[i];
+        resultDiv.append(p);
+
+    }
+
+}
+
+function clearItems() {
+    arr = [];
+    localStorage.meuArr = JSON.stringify(arr);
+
+}
